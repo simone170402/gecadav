3c741a18a130361e9e364e65fdfbf63b752492ff
TsapyNingahi SimoneArmandine <simone-armandine.tsapy-ningahi6@etu.univ-lorraine.fr>
2025-11-12 11:06:07 +0100

    fix: correct style.css and index.html

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./src/**/*.{html,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gold: '#bfa14a',
+        charcoal: '#2a2a2a',
+        cream: '#f9f9f9',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
   },
-  plugins: [],
-}
+};
