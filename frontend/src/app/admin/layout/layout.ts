f4dd172d941883fe5a6a8514f52b8e720cb652ee
TsapyNingahi SimoneArmandine <simone-armandine.tsapy-ningahi6@etu.univ-lorraine.fr>
2025-11-12 09:14:22 +0100

    Add Netlify routerlink in layout

--- frontend/src/app/admin/layout/layout.ts
+++ frontend/src/app/admin/layout/layout.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
