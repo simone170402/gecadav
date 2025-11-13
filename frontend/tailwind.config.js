78bd5129e800c9edd3556293e9000229e98b7821
TsapyNingahi SimoneArmandine <simone-armandine.tsapy-ningahi6@etu.univ-lorraine.fr>
2025-11-13 09:18:44 +0100

    update

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
-    "./**/*.{html,ts}", // <-- ajoute cette ligne
+    "./src/index.html",          // index global Angular
+    "./src/app/**/*.{html,ts}",  // 🔥 toutes tes pages & tous tes composants (SITE + ADMIN)
+    "./src/**/*.{html,ts}"       // par sécurité, si tu ajoutes d’autres dossiers dans src
   ],
   theme: {
     extend: {},
