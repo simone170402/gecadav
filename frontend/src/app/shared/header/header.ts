7e548bda6d60862c3db44e8bbb8957c5af43747c
simone170402 <simonetsapy4@gmail.com>
2025-12-16 13:50:54 +0100

    update

--- frontend/src/app/shared/header/header.ts
+++ frontend/src/app/shared/header/header.ts
@@ -34,15 +34,6 @@ export class Header implements AfterViewInit {
       });
   }
 
-  changeLang(lang: 'en' | 'fr') {
-  const select = document.querySelector<HTMLSelectElement>('select.goog-te-combo');
-  if (select) {
-    select.value = lang;
-    select.dispatchEvent(new Event('change'));
-  }
-}
-
-
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
