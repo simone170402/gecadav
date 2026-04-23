d3dd974dcee9f33c6b01f481074c16e5ca501416
simone170402 <simonetsapy4@gmail.com>
2026-04-23 15:38:38 +0200

    update

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,20 +1,10 @@
 /// <reference types="@angular/localize" />
 
-import { bootstrapApplication, provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
+import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
-import { routes } from './app/app.routes';
+import { appConfig } from './app/app.config';
 
 import 'zone.js';
 
-bootstrapApplication(App, {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(withFetch()),
-    provideAnimationsAsync(),
-    provideClientHydration(withEventReplay())
-  ],
-}).catch(err => console.error(err));
+bootstrapApplication(App, appConfig)
+  .catch(err => console.error(err));
